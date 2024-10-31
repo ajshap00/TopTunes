508aa6ab39546bd6379134a5d308fc079af44d1a
Alex Shapiro <ajshap00@gmail.com>
2024-10-31 01:53:38 -0400

    update

--- poll/VoteForm.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from .models import Artist
-
-class VoteForm(forms.Form):
-    artist = forms.ModelChoiceField(queryset=Artist.objects.all(), empty_label="Choose an Artist")
