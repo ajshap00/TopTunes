78ff71986e08dfb2cf42f59dae7aa7dea5350cde
Alex Shapiro <ajshap00@gmail.com>
2024-10-30 23:54:50 -0400

    Update Settings

--- Goat/settings.py
+++ Goat/settings.py
@@ -1,4 +1,6 @@
 import os
+from decouple import config
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 """
 Django settings for Goat project.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
