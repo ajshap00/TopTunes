3300db096c6ff441155193f941fea058c422d98a
Alex Shapiro <ajshap00@gmail.com>
2024-10-31 02:29:16 -0400

    update

--- Goat/settings.py
+++ Goat/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 # Application definition
 INSTALLED_APPS = [
