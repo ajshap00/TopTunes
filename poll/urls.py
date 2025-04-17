260248e42f290b70ef2db6fbb730f21defc925ac
Alex Shapiro <ajshap00@gmail.com>
2025-04-17 18:05:15 -0400

    updating homepage

--- poll/urls.py
+++ poll/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'poll'
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    path('', views.artist_page, name='home'),
     path('artists/<slug:slug>/', views.artist_detail, name='artist_detail'),
     path('artists/', views.artist_page, name='artists'),
     path('vote/', views.vote_page, name='vote'),
